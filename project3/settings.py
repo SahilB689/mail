40978c7f940f1464e4b728f68bc3d25439933f3a
SahilB689 <76981086+SahilB689@users.noreply.github.com>
2021-06-23 19:23:17 -0400

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ] 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
